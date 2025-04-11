eb3391c3c4165d0e173f6dcb51bf694db8eeaf65
Maksim <maxim2019mail.ua@gmail.com>
2025-04-12 00:22:20 +0300

    solving the problem with root paths

--- lab2/Scripts/Controller/ControllerProfile.js
+++ lab2/Scripts/Controller/ControllerProfile.js
@@ -12,10 +12,10 @@ export default class ControllerProfile {
             ProfileButton.addEventListener("click", (event) => {
                 event.preventDefault();
                 if (this.model.IsLoginIn()){
-                    window.location.href = "/lab2/Pages/profile.html";
+                    window.location.href = "/WEB_Lab2/lab2/Pages/profile.html";
                 }
                 else{
-                    window.location.href = "/lab2/Pages/signup.html";
+                    window.location.href = "/WEB_Lab2/lab2/Pages/signup.html";
                     this.view.showMessageError();
                 }
 
